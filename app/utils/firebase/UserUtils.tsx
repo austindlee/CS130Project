584433f181069a063d85dcfea802ebd1abf79dc3
Benson Han <bensonjaminhan@gmail.com>
2018-11-15 20:59:20 -0800

    add Typedocs and make createUser async

--- app/utils/firebase/UserUtils.tsx
+++ app/utils/firebase/UserUtils.tsx
@@ -4,17 +4,25 @@ import GlobalStyles from '../globals/GlobalStyles';
 import firebase, { firestore } from 'firebase';
 import 'firebase/firestore';
 
-export default function createUser(username: string) {
+/**
+ * Creates a user document in the Firestore no-sql database
+ * @param username - username - name of the user
+ */
+export default async function createUser(username: string) {
     const db = firebase.firestore();
     const settings = {
         timestampsInSnapshots: true
     };
     db.settings(settings);
-    db.collection('users').add({
+    await db.collection('users').add({
         username
     })
 }
 
+/**
+ * Gets the groups that a user is in
+ * @param userID - userID - takes a unique user ID to identify the user
+ */
 export default function getUsersGroups(userID: number) {
     const db = firebase.firestore();
     const settings = {
