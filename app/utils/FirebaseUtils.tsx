61d6c7f45212ef28437ae53940f803b0347b08d8
Austin <austindlee997@gmail.com>
2018-11-15 23:15:17 -0800

    finished group functions

--- app/utils/FirebaseUtils.tsx
+++ app/utils/FirebaseUtils.tsx
@@ -2,3 +2,7 @@ import React from 'react';
 import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 import GlobalStyles from '../globals/GlobalStyles';
 import firebase from 'firebase';
+export default function randInt(min,max) // min and max included
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
