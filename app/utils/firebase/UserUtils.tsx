28e71baedb4ee0738af2f76a8e9c83478c0c57b5
Benson Han <bensonjaminhan@gmail.com>
2018-11-15 21:29:06 -0800

    return correct values for database user utility functions

--- app/utils/firebase/UserUtils.tsx
+++ app/utils/firebase/UserUtils.tsx
@@ -5,35 +5,43 @@ import firebase, { firestore } from 'firebase';
 import 'firebase/firestore';
 
 /**
- * Creates a user document in the Firestore no-sql database
+ * Creates a user document in the Firestore no-sql database and returns the unique userID
  * @param username - username - name of the user
  */
 export default async function createUser(username: string) {
+    // initialize Firestore
     const db = firebase.firestore();
     const settings = {
         timestampsInSnapshots: true
     };
     db.settings(settings);
-    await db.collection('users').add({
+
+    // add user to collection
+    return await db.collection('users').add({
         username
-    })
+    }) 
 }
 
 /**
- * Gets the groups that a user is in
+ * Returns an array of groups that a user is in
  * @param userID - userID - takes a unique user ID to identify the user
  */
 export default function getUsersGroups(userID: number) {
+    // initialize Firestore
     const db = firebase.firestore();
     const settings = {
         timestampsInSnapshots: true
     };
     db.settings(settings);
+
+    // add each group from user's group subcollection to array
+    var groups : string[] = [];
     db.collection('users').doc(userID).collection('groups').get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            console.log(doc.id, " => ", doc.data());
+            groups.push(doc.id);
         });
     })
+    return groups;
 }
 
 // export default function deleteUser(userID: number) {
