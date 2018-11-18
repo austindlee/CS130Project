494438e954b2717d9ee1876ea23e33b80863d895
Michael <myque@live.com>
2018-11-18 11:31:46 -0800

    fix getUserGroups and indenting

--- app/utils/firebase/UserUtils.tsx
+++ app/utils/firebase/UserUtils.tsx
@@ -10,40 +10,45 @@ import 'firebase/firestore';
  * @return - the unique userID of the newly created user
  */
 export async function createUser(username: string) {
-    // initialize Firestore
-    const db = firebase.firestore();
-    const settings = {
-        timestampsInSnapshots: true
-    };
-    db.settings(settings);
+  // initialize Firestore
+  const db = firebase.firestore();
+  const settings = {
+    timestampsInSnapshots: true
+  };
+  db.settings(settings);
 
-    // add user to collection
-    return await db.collection('users').add({
-        username
-    }) 
+  // add user to collection
+  return await db.collection('users').add({
+    username
+  })
 }
 
 /**
  * Gets the groups that a user is currently a part of
  * @param userID - takes a unique user ID to identify the user
- * @return - an array of groups that the user is currently a part of 
+ * @return - an array of group IDs that the user is currently a part of
  */
-export async function getUsersGroups(userID: number) {
-    // initialize Firestore
-    const db = firebase.firestore();
-    const settings = {
-        timestampsInSnapshots: true
-    };
-    db.settings(settings);
+export async function getUsersGroups(userID: string) {
+  // initialize Firestore
+  const db = firebase.firestore();
+  const settings = {
+    timestampsInSnapshots: true
+  };
+  db.settings(settings);
 
-    // add each group from user's group subcollection to array
-    var groups : string[] = [];
-    await db.collection('users').doc(userID).collection('groups').get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            groups.push(doc.id);
-        });
-    })
-    return groups;
+  // add each group from user's group subcollection to array
+  let groups : string[] = [];
+  await db.collection('users').doc(userID).get().then((doc) => {
+    if(doc.exists) {
+      doc.data().groups.forEach((groupId: string) => {
+        groups.push(groupId);
+      })
+    }
+    else {
+      console.log("Can't find user");
+    }
+  });
+  return groups;
 }
 
 // export default function deleteUser(userID: number) {
