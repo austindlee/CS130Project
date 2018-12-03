6afb5cffcaad89795895ca97ec37778789b2e54e
Nicholas Turk <nturk19@ucla.edu>
2018-12-03 00:48:26 -0800

    Forgot to commit new tests

---
+++ app/tests/UserCalendarID.test.tsx
@@ -0,0 +1,25 @@
+import firebase, { firestore } from 'firebase';
+import 'firebase/firestore';
+
+const db = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true
+};
+db.settings(settings);
+
+
+test('the user has a calendarID in Firebase', () => {
+	expect.assertions(1);
+	return expect(await db.collection('users').doc(await Expo.SecureStore.setItemAsync('localUserID', userId.toString())).get().then((doc) => {
+    if(doc.exists) {
+        let calendarID = doc.data().userCalendarId;
+        if (calendarID == undefined || calendarID == null){
+          return null;
+        }
+        return true;
+      }
+    else {
+        return null;
+      }
+  }).toBeCalledWith(expect.anything());
+});
---
+++ app/tests/UserRefreshToken.test.tsx
@@ -0,0 +1,25 @@
+import firebase, { firestore } from 'firebase';
+import 'firebase/firestore';
+
+const db = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true
+};
+db.settings(settings);
+
+
+test('the user has a refreshToken in Firebase', () => {
+	expect.assertions(1);
+	return expect(await db.collection('users').doc(await Expo.SecureStore.setItemAsync('localUserID', userId.toString())).get().then((doc) => {
+    if(doc.exists) {
+        let refreshToken = doc.data().refreshToken;
+        if (refreshToken == undefined || refreshToken == null){
+          return null;
+        }
+        return true;
+      }
+    else {
+        return null;
+      }
+  }).toBeCalledWith(expect.anything());
+});
