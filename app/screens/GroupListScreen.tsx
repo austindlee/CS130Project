7d4883941a2104b3169e928b0b264bfe491d9866
Nicholas Turk <nturk19@ucla.edu>
2018-12-02 17:27:02 -0800

    On group list screen load, check each group and update their calendarIDs lists in Firebase

--- app/screens/GroupListScreen.tsx
+++ app/screens/GroupListScreen.tsx
@@ -35,10 +35,12 @@ class GroupListScreen extends React.Component<GroupListScreenProps, GroupListScr
     })
     const groupArray = await Promise.all(groupArrayPromises);
     this.setState({groupData: groupArray, isLoading: false});
-    //console.log("GROUP DATA: ", groupIDArray)
-    //console.log("GROUP DATA users: ", this.state.groupData[0].users)
-    await getUsersCalendarID(this.state.groupData[0].users, groupIDArray[0]); //get list of Calendar IDs for all members in the Group, then send this list to Firebase
-
+    console.log("GROUP DATA: ", groupIDArray)
+    console.log("GROUP DATA users: ", this.state.groupData[0].users)
+    for (var i in groupIDArray){
+      console.log("Updating data().calendarIDs for ", groupIDArray[i])
+      await getUsersCalendarID(this.state.groupData[0].users, groupIDArray[i]); //get list of Calendar IDs for all members in the Group, then send this list to Firebase
+    }
   }
 
   async componentWillReceiveProps(nextProps) {
