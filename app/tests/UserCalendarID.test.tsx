69669d24e990e7de32ba127fe7201b96033ef108
Nicholas Turk <nturk19@ucla.edu>
2018-12-03 00:58:02 -0800

    minor fix

--- app/tests/UserCalendarID.test.tsx
+++ app/tests/UserCalendarID.test.tsx
@@ -11,7 +11,7 @@ db.settings(settings);
 
 test('the user has a calendarID in Firebase', () => {
 	expect.assertions(1);
-	return expect(await db.collection('users').doc(await Expo.SecureStore.setItemAsync('localUserID', userId.toString())).get().then((doc) => {
+	return expect(await db.collection('users').doc(await Expo.SecureStore.getItemAsync('localUserID')).get().then((doc) => {
     if(doc.exists) {
         let calendarID = doc.data().userCalendarId;
         if (calendarID == undefined || calendarID == null){
